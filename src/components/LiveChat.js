4bc4539bf1738b72a596df77fbc1aec03e628972
Asif <67794014+nortaip@users.noreply.github.com>
2023-01-22 04:45:02 +0400

    22.01.2023

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Layout, Empty, Button, Input, Space } from 'antd';
+const { Content } = Layout;
 
 function LiveChat() {
-
+    const [value, setValue] = useState('99');
     return (
         <>
-            <div className='inf--o'
-                style={{
-                    minWidth: "390px"
-                }} >
-            </div>
+            <Layout style={{ minWidth: "390px", height: "100%" }}  >
+                <div className="Chat-dexsffa">
+                    <header>
+                        <div className='chahesfkt'>
+                            <h3>LiveChat</h3>
+                            <div className='bedgedsgs'>15</div>
+                        </div>
+                    </header>
+                    <Content className='chat-container'>
+                        <Empty description={false} />
+                    </Content>
+                    <footer>
+                        <div className='ant-space-itemhdf'>
+                            <Input placeholder='Send somthng' style={{ width: "100%" }} size='middle' />
+                            <Button
+                                type="primary"
+                                onClick={() => {
+                                    setValue(99);
+                                }}
+                            >
+                                send
+                            </Button>
+                        </div>
+                    </footer>
+                </div>
+            </Layout>
         </>
     );
 }
