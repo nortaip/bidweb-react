08f3dbacfb3a212372351705833893798c749f09
Asif <67794014+nortaip@users.noreply.github.com>
2023-04-02 10:03:12 +0400

    update

--- src/components/SellItems/SellEhtiyath.js
+++ src/components/SellItems/SellEhtiyath.js
@@ -236,7 +236,7 @@ function SellingItem() {
                                         </Form.Item>
                                     </Col>
                                 </Row>
-                                 {/* Mühərrikin həcmı , VIN-kod*/}
+                                {/* Mühərrikin həcmı , VIN-kod*/}
                                 <Row gutter={[16, 16]}>
                                     <Col span={8}>
                                         <Form.Item
