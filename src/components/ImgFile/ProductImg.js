79d9855584084cb68ed6efb16246ebee7df85ffb
Asif <67794014+nortaip@users.noreply.github.com>
2023-05-29 15:39:42 +0400

    img carsel update

--- src/components/ImgFile/ProductImg.js
+++ src/components/ImgFile/ProductImg.js
@@ -1,41 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
-import { Image } from 'antd';
-import { CONN_KEY } from "../../Conn";
-import axios from 'axios';
+import { Carousel } from 'react-carousel-minimal';
 
-function ProductImg() {
-    const [visible, setVisible] = useState(false);
-    const [brands, setBrands] = useState([]);
-    const { productId } = useParams();
+function App() {
+    const data = [
+        {
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+        },
+        {
+            image: "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+        },
+        {
+            image: "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
+        },
+        {
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
+        },
+        {
+            image: "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
+        },
+        {
+            image: "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
+        },
+        {
+            image: "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
+        },
+        {
+            image: "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
+        },
+        {
+            image: "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
+        }
+    ];
 
-    useEffect(() => {
-        const apiUrl = `${CONN_KEY}sellimg.php?id=${productId}`;
-        axios.get(apiUrl)
-            .then(response => {
-                setBrands(response.data);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }, [productId]);
-
+    const slideNumberStyle = {
+        fontSize: '20px',
+        fontWeight: 'bold',
+    }
     return (
-        <div className='Product-imgs'>
-            <Image.PreviewGroup
-                preview={{
-                    visible,
-                    onVisibleChange: (vis) => setVisible(vis),
-                }}
-            >
-
-                <Image className='main-img main-img-border' src={`../../uploads/6433a5d05e554/o_1fuqtt6ac1sn51fk19uk6501ksug.jpg`} />
-
-            </Image.PreviewGroup>
+        <div className="App">
+            <div style={{ textAlign: "center" }}>
+                <div style={{
+                    padding: "0 20px"
+                }}>
+                    <Carousel
+                        data={data}
+                        time={2000}
+                        width="1200px"
+                        height="400px"
+                        radius="10px"
+                        // slideNumber={true}
+                        slideNumberStyle={slideNumberStyle}
+                        // automatic={true}
+                        // dots={true}
+                        pauseIconColor="white"
+                        pauseIconSize="40px"
+                        slideBackgroundColor="darkgrey"
+                        slideImageFit="cover"
+                        thumbnails={true}
+                        thumbnailWidth="100px"
+                        style={{
+                            textAlign: "center",
+                            maxWidth: "850px",
+                            maxHeight: "500px",
+                            margin: "40px auto",
+                        }}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
 
-export default ProductImg;
+export default App;
