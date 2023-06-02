c6c1c888b3050c283a6b421a52a744aad5b9ace3
Asif <67794014+nortaip@users.noreply.github.com>
2023-06-02 22:01:43 +0400

    prodduct img update

--- src/components/ImgFile/ProductImg.js
+++ src/components/ImgFile/ProductImg.js
@@ -1,71 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Carousel } from 'react-carousel-minimal';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import axios from 'axios';
-import { CONN_KEY } from "../../Conn";
+import { CONN_KEY } from '../../Conn';
+import { Image } from 'antd';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper';
 
 function App() {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const { productId } = useParams();
+  const { productId } = useParams();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const apiUrl = `${CONN_KEY}getimgsPostId.php?post_id=${productId}`;
-                const response = await axios.get(apiUrl);
-                setData(response.data);
-                setIsLoading(false);
-            } catch (error) {
-                console.error(error);
-                setIsLoading(false);
-            }
-        };
-
-        fetchData();
-    }, [productId]);
-
-    const slideNumberStyle = {
-        fontSize: '20px',
-        fontWeight: 'bold',
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiUrl = `${CONN_KEY}getimgsPostId.php?post_id=${productId}`;
+        const response = await axios.get(apiUrl);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
     };
 
-    return (
-        <div className="App">
-            {isLoading ? (
-                <div>Loading...</div>
-            ) : (
-                <div style={{ textAlign: 'center' }}>
-                    <div style={{ padding: '0 20px' }}>
-                        <Carousel
-                            data={data.map(item => ({
-                                image: `${CONN_KEY}uploads/${item.folder_name}/${item.image_name}`,
-                                caption: item.caption
-                            }))}
-                            time={2000}
-                            width="1200px"
-                            height="400px"
-                            radius="10px"
-                            slideNumberStyle={slideNumberStyle}
-                            pauseIconColor="white"
-                            pauseIconSize="40px"
-                            slideBackgroundColor="darkgrey"
-                            slideImageFit="cover"
-                            thumbnails={true}
-                            thumbnailWidth="100px"
-                            style={{
-                                textAlign: 'center',
-                                maxWidth: '850px',
-                                maxHeight: '500px',
-                                margin: '40px auto',
-                            }}
-                        />
-                    </div>
-                </div>
-            )}
+    fetchData();
+  }, [productId]);
+
+  return (
+    <div className="App">
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div style={{ textAlign: 'center' }}>
+          <div style={{ padding: '0 20px' }}>
+            <Swiper
+              slidesPerView="auto"
+              spaceBetween={30}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              {data.map((item) => (
+                <SwiperSlide key={item.image_name}>
+                  <div
+                    className="image-slide"
+                    style={{
+                      backgroundImage: `url(${CONN_KEY}uploads/${item.folder_name}/${item.image_name})`                     
+                    }}
+                  />
+                  <Image
+                    src={`${CONN_KEY}uploads/${item.folder_name}/${item.image_name}`}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default App;
